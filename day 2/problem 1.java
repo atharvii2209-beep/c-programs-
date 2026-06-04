2ae54bf6217dc32fbfff04a87646cbfcdcfd0971
atharvii2209-beep <atharvii2209@gmail.com>
2026-06-04 15:51:39 +0530

    program to find sum of digits of a number

--- day 2/problem 1.java
+++ day 2/problem 1.java
@@ -1,1 +1,29 @@
 
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num, sum = 0, rem;
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input the number
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        // Handle negative numbers
+        if (num < 0) {
+            num = -num;
+        }
+
+        // Loop to calculate sum of digits
+        for (; num > 0; num = num / 10) {
+            rem = num % 10;   // extract last digit
+            sum += rem;       // add to sum
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
+    }
+}
