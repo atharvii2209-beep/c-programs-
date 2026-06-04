0fba81efe62e4b7fa10f383fe7d53dba22c4f8ec
atharvii2209-beep <atharvii2209@gmail.com>
2026-06-04 16:17:07 +0530

    print the multiplication of a given number

--- day 1/problem2.c
+++ day 1/problem2.c
@@ -1,18 +1,18 @@
+
 #include <stdio.h>
 
 int main() {
-    int N, sum = 0;
+    int num, i;
 
-    // Input the value of N
-    printf("Enter the value of N: ");
-    scanf("%d", &N);
+    // Input the number
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    // Using formula: sum = N * (N + 1) / 2
-    sum = N * (N + 1) / 2;
-
-    // Output the result
-    printf("The sum of the first %d natural numbers is: %d\n", N, sum);
+    // Print multiplication table
+    printf("Multiplication Table of %d:\n", num);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
 
     return 0;
 }
-
