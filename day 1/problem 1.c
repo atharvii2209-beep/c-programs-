84b8be0249683182cb0870f04cba6731945948d6
atharvii2209-beep <atharvii2209@gmail.com>
2026-06-04 13:13:25 +0530

    Implement sum of first N natural numbers

--- day 1/problem 1.c
+++ day 1/problem 1.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+
+int main() 
+{
+    int N, sum = 0;
+
+    // Input the value of N
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    // Using formula: sum = N * (N + 1) / 2
+    sum = N * (N + 1) / 2;
+
+    // Output the result
+    printf("The sum of the first %d natural numbers is: %d\n", N, sum);
+
+    return 0;
+}
