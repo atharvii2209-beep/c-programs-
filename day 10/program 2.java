d40e756eae51c8c8fd9477fa82efd8cbee8e4888
atharvii2209-beep <atharvii2209@gmail.com>
2026-06-20 11:16:53 +0530

    program to print reverse star pyramid pattern

---
+++ day 10/program 2.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = rows; i >= 1; i--) {
+            // print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            // print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
